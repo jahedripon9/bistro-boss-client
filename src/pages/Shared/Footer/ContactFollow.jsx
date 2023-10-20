f1c6ad8119a4aa2db03d5a3eb929c4c85a21d5fa
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2023-10-21 01:42:29 +0600

    Updating

---
+++ src/pages/Shared/Footer/ContactFollow.jsx
@@ -0,0 +1,53 @@
+
+const ContactFollow = () => {
+    return (
+        <div>
+            <section class="text-gray-600 body-font ">
+                <div class="container flex flex-wrap px-5 py-1 mx-auto items-center justify-center">
+                    <div class="md:w-1/2 md:pr-12 md:py-8 md:border-r md:border-b-0 mb-10 md:mb-0 pb-10 border-b border-gray-200 text-center">
+                        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">CONTACT US</h1>
+                        <p class="leading-relaxed text-base text-white">123 ABS Street, Uni 21, Bangladesh <br />
+                            +88 123456789 <br />
+                            Mon - Fri: 08:00 - 22:00 <br />
+                            Sat - Sun: 10:00 - 23:00</p>
+                        
+                    </div>
+                    <div class="flex flex-col md:w-1/2 md:pl-12">
+                        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">Follow US</h1>
+                        <nav class=" list-none -mb-1 text-white">
+                            <p>Join us on social media</p> 
+                            <div>
+                                <span class="flex sm:mx-auto justify-start my-5  ">
+                                    <a class="text-white">
+                                        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                                        </svg>
+                                    </a>
+                                    <a class="ml-3 text-white">
+                                        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                                        </svg>
+                                    </a>
+                                    <a class="ml-3 text-white">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                                            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                                        </svg>
+                                    </a>
+                                    <a class="ml-3 text-white">
+                                        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
+                                            <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+                                            <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                                        </svg>
+                                    </a>
+                                </span>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default ContactFollow;
